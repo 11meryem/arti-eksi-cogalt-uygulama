8af40c93f3d5c98af209431781b3f2c690cb5bc2
11meryem <145452149+11meryem@users.noreply.github.com>
2023-10-10 14:56:11 +0300

    Add files via upload

---
+++ button.24/button.24/button.24/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace button._24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnArti = new System.Windows.Forms.Button();
+            this.lblsayac = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEksi.Location = new System.Drawing.Point(28, 54);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(89, 75);
+            this.btnEksi.TabIndex = 0;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = true;
+            this.btnEksi.Click += new System.EventHandler(this.btnEksi_Click);
+            // 
+            // btnArti
+            // 
+            this.btnArti.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnArti.Location = new System.Drawing.Point(204, 54);
+            this.btnArti.Name = "btnArti";
+            this.btnArti.Size = new System.Drawing.Size(89, 75);
+            this.btnArti.TabIndex = 1;
+            this.btnArti.Text = "+";
+            this.btnArti.UseVisualStyleBackColor = true;
+            this.btnArti.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblsayac
+            // 
+            this.lblsayac.AutoSize = true;
+            this.lblsayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsayac.Location = new System.Drawing.Point(141, 82);
+            this.lblsayac.Name = "lblsayac";
+            this.lblsayac.Size = new System.Drawing.Size(86, 31);
+            this.lblsayac.TabIndex = 2;
+            this.lblsayac.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 266);
+            this.Controls.Add(this.lblsayac);
+            this.Controls.Add(this.btnArti);
+            this.Controls.Add(this.btnEksi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEksi;
+        private System.Windows.Forms.Button btnArti;
+        private System.Windows.Forms.Label lblsayac;
+    }
+}
+
---
+++ button.24/button.24/button.24/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac=0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            lblsayac.Text = sayac.ToString();
+            if (sayac < 10)
+            {
+                sayac++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblsayac.Text = "0";
+        }
+
+        private void btnEksi_Click(object sender, EventArgs e)
+        {
+            
+            lblsayac.Text=sayac.ToString();
+            if (sayac> -10)
+            {
+                sayac--;
+            }
+        }
+    }
+}
